d0be0b12abee1ec3ad909e22d970bb8a772b9958
Andy Saul <andycsaul@gmail.com>
2020-12-10 10:23:59 +0000

    Add Server struct

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
 fn main() {
-    println!("Hello, world!");
+
+    let server = Server::new("127.0.0.1:8080".to_string());
+    server.run();
+}
+
+struct Server {
+    address: String, 
+}
+
+impl Server {
+    fn new(address: String)-> Self {
+        Self { address }
+    }
+
+    fn run(self) {
+        println!("listening on {}", self.address);
+    }
 }
