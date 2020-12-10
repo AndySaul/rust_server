4789c5fe959578cd9c853ddc37d27adb234bfefc
Andy Saul <andycsaul@gmail.com>
2020-12-10 19:30:39 +0000

    Add server and method modules

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,10 @@
-fn main() {
+use http::Request;
+use server::Server;
 
+mod server;
+mod http;
+
+fn main() {
     let server = Server::new("127.0.0.1:8080".to_string());
     server.run();
-}
-
-struct Server {
-    address: String, 
-}
-
-impl Server {
-    fn new(address: String)-> Self {
-        Self { address }
-    }
-
-    fn run(self) {
-        println!("listening on {}", self.address);
-    }
 }
