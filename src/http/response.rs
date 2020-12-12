60b4c78c3a4183b9cd6b9c7d8599b6e5a2f61558
Andy Saul <andycsaul@gmail.com>
2020-12-12 21:38:09 +0000

    Remove unused imports

--- src/http/response.rs
+++ src/http/response.rs
@@ -1,7 +1,5 @@
 use super::StatusCode;
-use std::fmt::{Display, Formatter, Result as FmtResult};
 use std::io::{Result as IoResult, Write};
-use std::net::TcpStream;
 
 #[derive(Debug)]
 pub struct Response {
