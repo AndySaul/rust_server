500bca7247e05423326c95ee5332c5eb92311e77
Andy Saul <andycsaul@gmail.com>
2020-12-13 10:59:22 +0000

    Serve HTML pages

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use server::Server;
+use std::env;
 use website_handler::WebsiteHandler;
 
 mod http;
@@ -6,6 +7,9 @@ mod server;
 mod website_handler;
 
 fn main() {
+    let default_path = format!("{}/public", env!("CARGO_MANIFEST_DIR"));
+    let public_path = env::var("PUBLIC_PATH").unwrap_or(default_path);
+
     let server = Server::new("127.0.0.1:8080".to_string());
-    server.run(WebsiteHandler);
+    server.run(WebsiteHandler::new(public_path));
 }
