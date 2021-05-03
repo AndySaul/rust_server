474dea0211b4381ab723f0e1173ccf2f46c36cde
Andy Saul <andycsaul@gmail.com>
2021-05-03 17:27:08 +0100

    Extract handle_request function to reduce nesting

--- src/server.rs
+++ src/server.rs
@@ -1,7 +1,7 @@
 use crate::http::{ParseError, Request, Response, StatusCode};
 use std::convert::TryFrom;
 use std::io::Read;
-use std::net::TcpListener;
+use std::net::{TcpListener, TcpStream};
 
 pub trait Handler {
     fn handle_request(&mut self, request: &Request) -> Response;
@@ -9,32 +9,6 @@ pub trait Handler {
     fn handle_bad_request(&mut self, e: &ParseError) -> Response {
         println!("Failed to parse request: {}", e);
         Response::new(StatusCode::BadRequest, None)
-    }
-}
-
-pub fn wait_for_connection(handler: &mut impl Handler, listener: &mut TcpListener) {
-    match listener.accept() {
-        Err(e) => println!("Failed to establish connection: {}", e),
-
-        Ok((mut stream, _)) => {
-            let mut buffer = [0; 1024];
-            match stream.read(&mut buffer) {
-                Err(e) => println!("Failed to read from connection: {}", e),
-
-                Ok(_) => {
-                    println!("Received a request: {}", String::from_utf8_lossy(&buffer));
-
-                    let response = match Request::try_from(&buffer[..]) {
-                        Ok(request) => handler.handle_request(&request),
-                        Err(e) => handler.handle_bad_request(&e),
-                    };
-
-                    if let Err(e) = response.send(&mut stream) {
-                        println!("Failed to send response: {}", e);
-                    }
-                }
-            }
-        }
     }
 }
 
@@ -54,6 +28,34 @@ impl Server {
 
         loop {
             wait_for_connection(&mut handler, &mut listener);
+        }
+    }
+}
+
+pub fn wait_for_connection(handler: &mut impl Handler, listener: &mut TcpListener) {
+    match listener.accept() {
+        Err(e) => println!("Failed to establish connection: {}", e),
+
+        Ok((mut stream, _)) => handle_request(handler, &mut stream),
+    }
+}
+
+pub fn handle_request(handler: &mut impl Handler, stream: &mut TcpStream) {
+    let mut buffer = [0; 1024];
+    match stream.read(&mut buffer) {
+        Err(e) => println!("Failed to read from connection: {}", e),
+
+        Ok(_) => {
+            println!("Received a request: {}", String::from_utf8_lossy(&buffer));
+
+            let response = match Request::try_from(&buffer[..]) {
+                Ok(request) => handler.handle_request(&request),
+                Err(e) => handler.handle_bad_request(&e),
+            };
+
+            if let Err(e) = response.send(stream) {
+                println!("Failed to send response: {}", e);
+            }
         }
     }
 }
