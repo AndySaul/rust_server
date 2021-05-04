db2ebc7f293e4fbf5bf155bd3cba4d08cda954c2
Andy Saul <andycsaul@gmail.com>
2021-05-04 20:52:33 +0100

    Use a fake handler instead of real website

--- src/server_test.rs
+++ src/server_test.rs
@@ -1,5 +1,20 @@
 use super::*;
+use crate::http::{Request, Response, StatusCode};
 use std::io::ErrorKind;
+
+struct FakeHandler {}
+
+impl FakeHandler {
+    pub fn new() -> Self {
+        Self {}
+    }
+}
+
+impl server::Handler for FakeHandler {
+    fn handle_request(&mut self, _request: &Request) -> Response {
+        Response::new(StatusCode::Ok, Some("dummy".to_string()))
+    }
+}
 
 #[test]
 fn ip_address_is_stored_in_server() {
@@ -10,9 +25,10 @@ fn ip_address_is_stored_in_server() {
 
 #[test]
 fn does_not_run_with_invalid_ip_address() {
+    let handler = FakeHandler::new();
     let server = Server::new("not an ip address".to_string());
 
-    let error = server.run(Website::new("".to_string())).unwrap_err();
+    let error = server.run(handler).unwrap_err();
 
     assert_eq!(error.kind(), ErrorKind::InvalidInput);
 }
